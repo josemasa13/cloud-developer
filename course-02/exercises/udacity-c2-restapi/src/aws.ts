8acfa2d301a6b21f61a433854e25408a1e494a87
Marcial Sanchez Jose Alberto <jose.marcial@solistica.com>
2020-05-03 12:47:22 -0500

    First commit

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -7,6 +7,8 @@ const c = config.dev;
 var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
 
+
+
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: c.aws_region,
@@ -22,8 +24,7 @@ export const s3 = new AWS.S3({
  */
 export function getGetSignedUrl( key: string ): string{
 
-  const signedUrlExpireSeconds = 60 * 5
-
+    const signedUrlExpireSeconds = 60 * 5
     const url = s3.getSignedUrl('getObject', {
         Bucket: c.aws_media_bucket,
         Key: key,
